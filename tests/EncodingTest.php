46ae76a46c000fb30a53a9268e74b9cb7160a0f2
Will Norris <will@willnorris.com>
2009-01-29 15:41:47 -0800

    add unit tests from Morten Fangel

---
+++ tests/EncodingTest.php
@@ -0,0 +1,33 @@
+<?php
+
+require_once dirname(__FILE__) . '/../Auth/OAuth/Util.php';
+
+/**
+ * Test parameter encoding
+ *
+ * @see http://wiki.oauth.net/TestCases
+ */
+class EncodingTest extends PHPUnit_Framework_TestCase {	
+	public function testParameterEncoding() {
+		$parameters = array(
+			'abcABC123' => 'abcABC123',
+			'-._~'      => '-._~',
+			'%'         => '%25',
+			'+'         => '%2B',
+			'&=*'       => '%26%3D%2A',
+			"\x0A"      => '%0A',
+			"\x20"      => '%20',
+			"\x7F"      => '%7F',
+
+			// suggestion for Unicode ?
+			//"\u0080"    => '%C2%80',
+			//"\u3001"    => '%E3%80%81',
+		);
+
+		foreach ($parameters as $param => $encoded) {
+			$this->assertEquals($encoded, Auth_OAuth_Util::urlencode($param));
+		}
+	}
+}
+
+?>
