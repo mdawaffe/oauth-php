e517f1cc7eab42ac84852cc162208a146116b146
Will Norris <will@willnorris.com>
2009-02-02 12:02:20 -0800

    pull manual HMAC logic into separate (protected) function so we can test it directly

--- Auth/OAuth/SignatureMethod/HMAC_SHA1.php
+++ Auth/OAuth/SignatureMethod/HMAC_SHA1.php
@@ -38,25 +38,32 @@ class Auth_OAuth_SignatureMethod_HMAC_SHA1 implements Auth_OAuth_SignatureMethod
 		}
 		else
 		{
-		    $blocksize	= 64;
-		    $hashfunc	= 'sha1';
-		    if (strlen($key) > $blocksize)
-		    {
-		        $key = pack('H*', $hashfunc($key));
-		    }
-		    $key	= str_pad($key,$blocksize,chr(0x00));
-		    $ipad	= str_repeat(chr(0x36),$blocksize);
-		    $opad	= str_repeat(chr(0x5c),$blocksize);
-		    $hmac 	= pack(
-		                'H*',$hashfunc(
-		                    ($key^$opad).pack(
-		                        'H*',$hashfunc(
-		                            ($key^$ipad).$base_string
-		                        )
-		                    )
-		                )
-		            );
+			$hmac = self::manual_hmac('sha1', $base_string, $key);
 		}
+
+		return $hmac;
+	}
+
+
+	protected function manual_hmac ( $algorithm, $base_string, $key )
+	{
+		$blocksize	= 64;
+		if (strlen($key) > $blocksize)
+		{
+			$key = pack('H*', $algorithm($key));
+		}
+		$key	= str_pad($key,$blocksize,chr(0x00));
+		$ipad	= str_repeat(chr(0x36),$blocksize);
+		$opad	= str_repeat(chr(0x5c),$blocksize);
+		$hmac 	= pack(
+					'H*',$algorithm(
+						($key^$opad).pack(
+							'H*',$algorithm(
+								($key^$ipad).$base_string
+							)
+						)
+					)
+				);
 
 		return $hmac;
 	}
