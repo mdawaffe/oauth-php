50d03507ba7d56faf1d6a1622a10129881f7c849
Will Norris <will@willnorris.com>
2009-02-26 17:17:34 -0800

    placeholder Discovery class

---
+++ Auth/OAuth/Discovery.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * OAuth Discovery.  This class will eventually make use of, and possibly 
+ * extend, a common discovery libary that utilizes XRDS, XRD, or both.
+ */
+class Auth_OAuth_Discovery
+{
+
+}
+
+?>
